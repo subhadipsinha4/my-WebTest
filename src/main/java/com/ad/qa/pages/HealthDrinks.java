c7b3c85aa0dd66c9d9aadceb3d3a23e51b694d38
subhadipsinha4 <subhadipsinha4@yahoo.com>
2026-04-28 08:45:47 +0530

    Added sort by price test case

--- src/main/java/com/ad/qa/pages/HealthDrinks.java
+++ src/main/java/com/ad/qa/pages/HealthDrinks.java
@@ -4,8 +4,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class HealthDrinks {
@@ -21,6 +26,15 @@ public class HealthDrinks {
     @FindBy(xpath = "//div[@id='product-list-wrap']//h2")
     private List<WebElement> productNames;
 
+    @FindBy(xpath = "//span[@class='sort-name']")
+    private WebElement sortOption;
+
+    @FindBy(xpath = "//label[@for='radio_price: low to high_undefined']")
+    private WebElement sortLowToHigh;
+
+    @FindBy(xpath = "//div[@id='product-list-wrap']//span[@class='css-111z9ua']")
+    private List<WebElement> productPrice;
+
     public HealthDrinks getPageNameAndDetails(){
         String getTotalCount=pageTitle.getText();
         System.out.println("Total priduct on screen: "+getTotalCount.replaceAll(".*\\((\\d+)\\).*", "$1"));
@@ -33,6 +47,26 @@ public class HealthDrinks {
         for(WebElement e: productNames){
             System.out.println(e.getText());
         }
+        return this;
+    }
+
+    public HealthDrinks selectSortOption(){
+        sortOption.click();
+        sortLowToHigh.click();
+        return this;
+    }
+
+    public HealthDrinks checkProductShownAsPerSortedOrderOrNot() {
+        List<Integer> productPriceList=new ArrayList<>();
+        WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(50));
+        wait.until(ExpectedConditions.visibilityOfAllElements(productPrice));
+        for(WebElement e: productPrice){
+            productPriceList.add(Integer.valueOf(e.getText().replace("₹","")));
+        }
+        List<Integer> expectedPriceList=new ArrayList<>(productPriceList);
+        Collections.sort(expectedPriceList);
+        System.out.println("Price stored: "+productPriceList);
+        Assert.assertEquals(productPriceList,expectedPriceList,"Sort is not working");
         return this;
     }
 }
